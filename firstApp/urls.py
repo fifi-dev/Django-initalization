3021b281cc53cb609b9955cad392a94497f2923d
fidelineBazana <fideline.bazana@ooeo.fr>
2022-02-28 14:43:16 +0100

    CRUD read

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import detail_view
+
 
 app_name = 'firstApp'
 urlpatterns = [
   path('', views.index, name='index'),
   path('new', views.new_question, name='new_question'),
+  path('questions', views.questions_lit, name='questions_lit'),
+  path('<id>', detail_view ),
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -22,3 +22,22 @@ def new_question(request):
     context['form']= form
     return render(request, 'firstApp/new_question.html', context)
 
+def questions_lit(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+
+    # add the dictionary during initialization
+    context["dataset"] = Question.objects.all()
+    
+    return render(request, "firstApp/questions.html", context)
+
+# pass id attribute from urls
+def detail_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+
+    # add the dictionary during initialization
+    context["data"] = Question.objects.get(id = id)
+    return render(request, "firstApp/detail.html", context)
