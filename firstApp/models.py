ffc07e15c7ccb80ccd251018862cc01a66ae78c0
fidelineBazana <fideline.bazana@ooeo.fr>
2022-02-28 11:07:47 +0100

    fisrt app route

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,3 +1,9 @@
+from cgitb import text
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    def str(self) -> str:
+        return self.text
