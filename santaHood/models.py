b747a73c7b4f079d8b1c5abeaacf58a6dc5becff
fidelineBazana <fideline.bazana@ooeo.fr>
2022-03-01 11:05:19 +0100

    project setup and design

--- santaHood/models.py
+++ santaHood/models.py
@@ -7,5 +7,10 @@ class Product(models.Model):
     slug = models.SlugField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.CharField(max_length=5000, null=True, blank=True)
+    image = models.ImageField(upload_to = "santaHood/static/santaHood/img/", null=True, blank=True)
+    alt = models.CharField(max_length=50, null=True, blank=True)
     quantity = models.IntegerField()
+    last_modified = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
--- santaHood/views.py
+++ santaHood/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
+def index(request):
+  return render(request, 'santaHood/index.html')
+
+def cart(request):
+  context ={}
+  return render(request, 'santaHood/cart.html', context)
+
+def checkout(request):
+  context ={}
+  return render(request, 'santaHood/checkout.html', context)
+
+def shop(request):
+  context ={}
+  return render(request, 'santaHood/shop.html', context)
